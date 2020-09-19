22d69324274a09b6b631fd2531d7986d2ac34ce1
Le Thanh <home@Les-iMac.local>
2020-09-19 09:55:50 +0700

    initial project

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+	render() {
+		return (
+			<div className='search-bar ui segment'>
+				<form className='ui form'>
+					<div className='field'>
+						<label>Video Search</label>
+
+						<input type='text' />
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
